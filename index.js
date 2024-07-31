4d78cb37f4b910c8a9346be7a1f4970621ceb710
Vinicius Marques Ferreira <dev.viniciusmarques@gmail.com>
2024-07-31 19:08:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const axios = require('axios')
+const path = require('path');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// Crie uma instância da API
+const spotifyApi = new SpotifyWebApi({
+    clientId: '24604eb43ab4456e82f8aa73f9fccdb9',
+    clientSecret: 'd9e0f7ce222d4c19ab958acb8ed8dfed',
+    redirectUri: 'http://localhost:3000/callback' // Substitua pela sua URI de redirecionamento
+});
+
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+const port = 3000;
+
+const scopes = [
+    'user-read-private',
+    'user-read-email',
+    'user-top-read',
+    'playlist-modify-public',
+    'playlist-modify-private',
+    'playlist-read-private'
+];
+  
+app.get('/login', (req, res) => {
+    const authorizeUrl = spotifyApi.createAuthorizeURL(scopes);
+    res.redirect(authorizeUrl);
+});
+
+app.get('/callback', (req, res) => {
+    const code = req.query.code;
+   
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        spotifyApi.setAccessToken(data.body['access_token']);
+        spotifyApi.setRefreshToken(data.body['refresh_token']);
+
+        res.redirect('http://localhost:3000/home');
+    }).catch(err => {
+        console.error(err);
+        res.status(500).send('Error exchanging code for token');
+    });
+});
+
+app.post('/request', async (req, res) => {
+    await spotifyApi.addTracksToPlaylist(['2mS2nwyCxtANFsHzl6Mh8i'], [`spotify:track:${req.body.track_id}`]).then(async (data) => {
+        const track = await spotifyApi.getTrack(req.body.track_id)
+        res.send(track);
+    }, function(err) {
+        throw err;
+    });
+})
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); 
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,51 @@
+const pusher = new Pusher("787e05d557a8480c3ee7",{
+    cluster: "mt1"
+}).subscribe('916ab9aa-a659-4d92-871e-8abdcb92d2f2');
+
+pusher.bind("messages", (async function(data) {
+
+    // const mensagem = data['DonatorMessage'];
+    const mensagem = '!sr https://open.spotify.com/intl-pt/track/64BbK9SFKH2jk86U3dGj2P?si=d6523633eb814f44';
+    const isSongRequest = await verificaDonateSongRequest(mensagem)
+
+    if(isSongRequest) {
+        return new Promise(async (resolve, reject) => {
+            const isValidSpotifyLink = await verificaLinkSpotify(isSongRequest)
+            if(isValidSpotifyLink) resolve(isValidSpotifyLink)
+    
+            reject('Não é um link válido')
+        }).then((spotifyLink) => {
+            const track = spotifyLink.split('track/')[1].split('?')[0]
+    
+            addTrackToPlaylist(track)
+        })
+      }
+})) 
+
+async function verificaDonateSongRequest(donateText) {
+    if(donateText.includes('!sr')) {
+        return donateText.split('!sr ')[1]
+    } else {
+        return false;
+    }
+}
+
+async function verificaLinkSpotify(linkSongRequest) {
+    if(linkSongRequest.includes('spotify.com' && 'track')) {
+        return linkSongRequest;
+    } else {
+        return false;
+    }
+}
+
+async function addTrackToPlaylist(track) {
+    axios.post('http://localhost:3000/request', {track_id: track}).then(response => {
+        const ulRequest = document.getElementById('requests')
+        const liTrack = document.createElement('li')
+
+        liTrack.innerHTML = `${response.data.body.name} - ${response.data.body.artists[0].name}`;
+        ulRequest.appendChild(liTrack)
+    }).catch(err => {
+        console.log(err);
+    })
+}
